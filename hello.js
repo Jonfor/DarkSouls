69c02e6b1e28ebedc8412d293fee8327896826f8
jonfor <jonforscher@gmail.com>
2015-09-24 17:09:18 -0400

    My body is almost ready

--- hello.js
+++ hello.js
@@ -1,12 +1,35 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.darkChecker.events({
-    'click button': function () {
+  Template.bodyIsReadyTemplate.events({
+    'click #bodyIsReady': function () {
       new Audio('/start.mp3').play();
+      document.getElementById('bodyIsReady').setAttribute("disabled", "true");
+      document.getElementById('human').checked = false;
+      document.getElementById('ring').checked = false;
+      Session.set('isHuman', false);
+      Session.set('isRing', false);
+    },
+
+    'click #human': function (event) {
+      new Audio('parry.mp3').play();
+      Session.set('isHuman', event.target.checked);
+
+      isBodyReady();
+    },
+
+    'click #ring': function (event) {
+      new Audio('parry.mp3').play();
+      Session.set('isRing', event.target.checked);
+
+      isBodyReady();
     }
   });
+
+  function isBodyReady() {
+    if (Session.get('isHuman') && Session.get('isRing')) {
+      document.getElementById('bodyIsReady').removeAttribute("disabled");
+    }
+  }
 }
 
 if (Meteor.isServer) {
